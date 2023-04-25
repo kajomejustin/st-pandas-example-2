0415de5d6ed706026a48592d6e72aeeebbfb870f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-04-25 11:14:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🐼 Pandas - A minimum working example')
+st.title('🐼 Pandas - An EDA example')
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
 st.write(df)
